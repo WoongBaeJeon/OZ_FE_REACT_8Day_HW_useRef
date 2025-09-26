5c8cfc444515930d4c1987cf9c9ec658a430e5b3
jwb <jeonwb622@gmail.com>
2025-09-26 15:50:01 +0900

    component 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
-import { useRef, useState } from "react";
 import "./App.css";
+import SearchView from "./component/SearchView";
 
 function App() {
-  // 검색어: 상태(state)로 관리 -> 사용자에게 즉각적으로 UI를 보여주기 위해
-  const [searchText, setSearchText] = useState("");
-  const inputRef = useRef("");
-
-  const getSearchValue = (e) => {
-    // console.log(inputRef.current.value);
-    setSearchText(inputRef.current.value);
-  };
-
   return (
     <>
-      <h2>OZ 코딩스쿨</h2>
-      <p>검색할 키워드를 입력한 후 검색 버튼을 클릭하세요.</p>
-      <div className="search-input">
-        <input type="text" ref={inputRef} />
-        <button onClick={getSearchValue}>검색</button>
-      </div>
-      {searchText && (
-        <div>
-          🔎 <b>{searchText}</b> 에 대해 검색합니다 . . .
-        </div>
-      )}
+      <SearchView />
     </>
   );
 }
